e9a3cd7fe8cde19bffd0aae66956c0bbabc885b4
Antonshepitko <antonshepitko99@gmail.com>
2022-10-19 16:13:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,18 @@ def random_cell(board):
 ship_row = random_cell(board)
 ship_col = random_cell(board)
 
-guess_row = int(input("Guess Row:"))
-guess_col = int(input("Guess Col:"))
+
 # if the user's right, the game ends
-if guess_row == ship_row + 1 and guess_col == ship_col + 1:
-    print("Congratulations! You sunk my battleship!")
+for turn in range(4):
+    guess_row = int(input("Guess Row:"))
+    guess_col = int(input("Guess Col:"))
+    if guess_row == ship_row + 1 and guess_col == ship_col + 1:
+        print("Congratulations! You sunk my battleship!")
+        break
+    else:
+        print("You missed my battleship!")
+        board[guess_row - 1][guess_col - 1] = "X"
+        print("Turn " + str(turn + 1) + " out of 4.")
+        print_board(board)
+if turn >= 3:
+    print("Game Over")
