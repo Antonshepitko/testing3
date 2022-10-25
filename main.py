216af0aa528d6f69a0e0a32429c991f3a36679b9
Antonshepitko <antonshepitko99@gmail.com>
2022-10-25 22:57:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,24 @@ def get_cords(board):
 print("Let's play Battleship!")
 board = create_board()
 print_board(board)
+
+def shoot(row, col, guess_row, guess_col):
+    if guess_row == row + 1 and guess_col == col + 1:
+        print("Congratulations! You sunk my battleship!")
+        return ("Victory!")
+    else:
+        print("You missed my battleship!")
+        board[guess_row - 1][guess_col - 1] = 'X'
+        print_board(board)
+        return ("Miss")
+
+def main_game():
+    row = 1
+    col = 1
+    for i in range(4):
+        guess_row = input("Enter row: ")
+        guess_col = input("Enter column: ")
+        result = shoot(row, col, guess_row, guess_col)
+        if result == "Victory!":
+            return (0)
+    print ("Defeat")
